8371d3f0632783453f8658c15ddf6982b9314681
arturs706 <arturs706>
2024-05-26 11:19:56 +0100

    Initial Commit

---
+++ aipainapp/App/aipainappApp.swift
@@ -0,0 +1,17 @@
+//
+//  aipainappApp.swift
+//  aipainapp
+//
+//  Created by Arturs Radionovs on 26/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct aipainappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
